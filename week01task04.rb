e60bff7105e448b91ca9cd771867ed52bb12712b
makcmlk <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2017-05-23 22:44:40 +0300

    04 зададние 01 недели исправил замечания

--- week01task04.rb
+++ week01task04.rb
@@ -7,16 +7,16 @@ b = gets.chomp.to_f
 puts "Введите третий коэффициент"
 c = gets.chomp.to_f
 
-disc = b**2 - 4.0 * a * c
+disc = b**2 - 4 * a * c
 
 if disc < 0
   print "Нет корней :("
 elsif disc == 0
-  root = -b / 2.0 * a
+  root = -b / 2 * a
   print "Дискриминант = 0, единственный корень: #{root}"
 else
   disc_sqrt = Math.sqrt(disc)
-  first_root = (-b + disc_sqrt) / (2.0 * a)
-  second_root = (-b - disc_sqrt) / (2.0 * a)
+  first_root = (-b + disc_sqrt) / (2 * a)
+  second_root = (-b - disc_sqrt) / (2 * a)
   print "Дискриминант = #{disc}, первый корень: #{first_root}, второй корень: #{second_root}"
 end
