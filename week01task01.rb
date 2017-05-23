4a17b8bc813988761930fb86b6fdf14d2d883137
makcmlk <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2017-05-23 22:30:23 +0300

    01 зададние 01 недели исправил замечания

--- week01task01.rb
+++ week01task01.rb
@@ -4,9 +4,8 @@ name = gets.chomp
 puts "Пожалуйста введите свой рост"
 height = gets.chomp.to_i
 weight = height - 110
-case weight
-  when weight > 0
-    print "#{name}, Ваш идеальный вес - #{weight}!"
-  when height - 110 <= 0
-    print "#{name}, Ваш идеальный вес рассчитывается при помощи другой формулы!"
+if weight <= 0
+  print "#{name}, Ваш идеальный вес рассчитывается при помощи другой формулы!"
+else
+  print "#{name}, Ваш идеальный вес - #{weight}кг!"
 end
