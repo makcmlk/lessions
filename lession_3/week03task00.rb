8078050dc600c5e21c5348f3956cab0ec118eac1
makcmlk <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2017-07-02 18:51:30 +0300

    lession 3 task 0

--- lession_3/week03task00.rb
+++ lession_3/week03task00.rb
@@ -1,4 +1,47 @@
+
+
+class Train
+  attr_accessor :number
+  attr_accessor :type
+  def initialize(number, type, count_of_cars)
+    @number = number
+    @type = type
+    @count_of_cars = count_of_cars
+    @speed = 0
+  end
+
+  def route(route)
+    @route = route
+    "route of #{train.number} is changed"
+  end
+
+  def speed
+    puts "speed of train is #{@speed}"
+  end
+
+  def up_speed
+    puts 'Up speed! too! too!'
+    @speed += 60
+  end
+
+  def stop
+    puts 'Train is stoped'
+    @speed = 0
+  end
+
+  def add_car
+    if @speed.zero?
+      @count_of_cars += 1
+      puts "car is added. count of cars: #{@count_of_cars}"
+    else
+      puts 'can not add a car on the move'
+    end
+  end
+
+end
+
 class Station
+  attr_accessor :name
   def initialize(name)
     @name = name
     @list_of_trains = {}
@@ -9,14 +52,17 @@ class Station
   end
 
   def list_of_trains()
-    @list_of_trains.size
+    puts "List of trains type type #{type}"
+    @list_of_trains.each { |train, type| puts "train  No#{train}, type #{type}" }
   end
 
   def get_list_of_trains_by_type(type)
-    @list_of_trains
+    puts "List of trains type type #{type}"
+    @list_of_trains.invert.values_at(type).each { |x| puts "train number #{x}" }
   end
 
   def send_train(train)
+    puts "train #{train.number} is on the route"
     @list_of_trains.delete(train.number)
   end
 end
@@ -26,21 +72,22 @@ class Route
     @stations = [from, to]
   end
 
-  def add_station(station)
+  def add_station(station, order)
+    if order <= @stations.size
+      @stations.insert(order, station)
+      puts "#{station.name} is in the route"
+    else
+      puts 'Station is not in the route'
+    end
   end
 
   def delete_station(station)
-  end
-end
-
-class Train
-  def initialize(number, type, count_of_cars)
-    @number = number
-    @type = type
-    @count_of_cars = count_of_cars
+    puts "#{station.name} is deleted from route"
+    @stations.delete_if { |x| x == station }
   end
 
-  def route(route)
-    @route = route
+  def list
+    puts 'Stations in this route:'
+    @stations.each { |station| puts station.name }
   end
 end
