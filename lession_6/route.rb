0b6fe522aadee7eb49ab56578ab58c8fa90a1573
makcmlk <unitcircle@protonmail.comunitcircle@protonmail.comunitcircle@protonmail.com>
2017-10-17 16:21:50 +0300

    lession 6. the first try

---
+++ lession_6/route.rb
@@ -0,0 +1,47 @@
+class Route
+  def initialize(from, to)
+    @stations = [from, to]
+    validate!
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  def add_station(station, order)
+    if order <= @stations.size
+      @stations.insert(order, station)
+    else
+      nil
+    end
+  end
+
+  def delete_station(station)
+    @stations.delete_if { |x| x == station }
+  end
+
+  def list
+    list = ''
+    @stations.each_with_index { |station, index| list+="[#{index}] #{station.name}, " }
+    list
+  end
+
+  def station(number)
+    return @stations[number] unless @stations[number].nil?
+  end
+
+  def size
+    @stations.size
+  end
+
+  protected
+
+  def validate!
+    raise 'No or invalid start station' if @stations[0].nil? || @stations[0].class != Station
+    raise 'No or invalid finish station' if @stations[1].nil? || @stations[1].class != Station
+    true
+  end
+
+end
---
+++ lession_6/station.rb
@@ -0,0 +1,52 @@
+class Station
+  attr_accessor :name
+
+  @@stations = []
+
+  class << self
+    def all
+      @stations
+    end
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    @@stations.push(self)
+    validate!
+  end
+
+  def get_train(train)
+    if train.is_a?(Train)
+      @trains.push(train)
+    end
+  end
+
+  def send_train(train)
+    if train.is_a?(Train)
+      @trains.delete(train)
+    end
+  end
+
+  def list_of_trains
+    @trains
+  end
+
+  protected
+
+  def validate!
+    raise "Name of station can't be empty" if name.nil?
+    raise 'Name of station should be at least 3 symbols' if name.length < 3
+    true
+  end
+
+  private
+
+  # не используем этот метод
+  def list_of_trains_by(type)
+    puts "List of trains type #{type}"
+    @trains.each do |train|
+      puts "train  No #{train.number}, type #{train.type}" if train.type == type
+    end
+  end
+end
